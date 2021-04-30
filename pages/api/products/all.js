103d85f4fb696845a404bd3440419ed3bad0fff1
Thomas Lenormand <thomas_lenormand@hotmail.fr>
2021-05-01 00:44:37 +0200

    Change api/products/all to use async/await

--- pages/api/products/all.js
+++ pages/api/products/all.js
@@ -2,11 +2,11 @@ import connectDB from '../../../middlewares/mongodb';
 // import getT from 'next-translate/getT'
 import Product from '../../../models/Product';
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === 'GET') {
     // const t = await getT(req.query.__nextLocale, 'auth')
-    const productsDB = Product.find({}, function (err, products) {
-      res.json(products);
+    await Product.find({}).then(function (products) {
+      res.status(200).send(products);
     });
   }
   else {
