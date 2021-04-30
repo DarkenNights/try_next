0d949ebdddb7b280b94552a5be624cfca813fbee
Thomas Lenormand <thomas_lenormand@hotmail.fr>
2021-05-01 00:37:46 +0200

    Localization for FRONTEND

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Layout from '../components/layout'
+import useTranslation from 'next-translate/useTranslation'
 
 export default function Index() {
+
+  const { t } = useTranslation('auth');
+
   return (
     <Layout>
       <div>
         <h1>Vous êtes sur la page d'accueil</h1>
+        <h2>{t('auth.name.error.required')}</h2>
       </div>
     </Layout>
   )
